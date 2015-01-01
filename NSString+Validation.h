2dab6a10328c5e1951f8872816cf0e5cca8a947d
Init Commit <mrugrajsinh@gmail.com>
2015-01-01 10:38:16 +0530

    Init Commit

---
+++ NSString+Validation.h
@@ -0,0 +1,21 @@
+//
+//  NSString+Validation.h
+//  PasteboatiOS
+//
+//  Created by indianic on 29/12/14.
+//  Copyright (c) 2014 IndiaNIC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Validation)
+
+-(BOOL)isValidEmail;
+-(BOOL)isValidNumber;
+-(BOOL)isValidPhoneNumber;
+-(BOOL)isValidPassword;
+-(BOOL)isValidSecuredPassword;
+-(BOOL)isValidString;
+-(NSString*)trim;
+
+@end
